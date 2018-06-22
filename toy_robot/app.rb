f1a9da1c212ba73bf35ad54c34e14b6eb80df501
Steve Occhipinti <dev@stevenocchipinti.com>
2018-06-22 14:50:03 +1000

    Make the ASCII map dynamically generated

--- toy_robot/app.rb
+++ toy_robot/app.rb
@@ -1,41 +1,71 @@
-MAP_SIZE = [5, 5] # W x H
-ROBOT = {
-  height: 0,
-  width: 0,
-  direction: :north
+MAP = {
+  width: 5,
+  height: 5
 }
-TARGET = [4, 4]
+
+ROBOT = {
+  x: 0,
+  y: 0,
+  direction: :east
+}
+
+TARGET = {
+  x: 4,
+  y: 4
+}
 
 
 
 CSI = "\e["
 CLEAR = "#{CSI}H#{CSI}J"
 
-DIRECTIONS = {
+ARROWS = {
   west: '←',
   north: '↑',
   east: '→',
   south: '↓',
 }
 
-def print_frame(number_of_moves=0)
+
+def ascii_map(map:, robot:, target:)
+  width = map[:width]
+  height = map[:height]
+
+  top_border    = "    ┌#{ "───┬" * (width - 1) }───┐\n"
+  middle_border = "    ├#{ "───┼" * (width - 1) }───┤\n"
+  bottom_border = "    └#{ "───┴" * (width - 1) }───┘\n"
+  x_axis        = "      #{width.times.map.to_a.join("   ")}\n"
+
+  rows = height.times.map do |h|
+    row = height - 1 - h
+    y_axis = sprintf "%3d │", row
+    cells = width.times.map do |col|
+      if robot[:x] == row && robot[:y] == col
+        " #{ARROWS[robot[:direction]]} "
+      elsif target[:x] == row && target[:y] == col
+        " ⭑ "
+      else
+        "   "
+      end
+    end.join("│")
+    "#{y_axis}#{cells}│\n"
+  end
+
+  top_border + rows.join(middle_border) + bottom_border + x_axis
+end
+
+def print_frame(map:, robot:, target:, number_of_moves:)
   print CLEAR
   puts "Robot map:"
   puts
-  puts "    ┌───┬───┬───┬───┬───┐"
-  puts "  4 │   │   │   │   │ ⭑ │"
-  puts "    ├───┼───┼───┼───┼───┤"
-  puts "  3 │   │   │   │   │   │"
-  puts "    ├───┼───┼───┼───┼───┤"
-  puts "  2 │   │   │   │   │   │"
-  puts "    ├───┼───┼───┼───┼───┤"
-  puts "  1 │   │   │   │   │   │"
-  puts "    ├───┼───┼───┼───┼───┤"
-  puts "  0 │ → │   │   │   │   │"
-  puts "    └───┴───┴───┴───┴───┘"
-  puts "      0   1   2   3   4  "
+  puts ascii_map map: map, robot: robot, target: target
   puts
   puts "Moves: #{number_of_moves}"
 end
 
-print_frame 0
+print_frame(
+  map: MAP,
+  robot: ROBOT,
+  target: TARGET,
+  number_of_moves: 0
+)
