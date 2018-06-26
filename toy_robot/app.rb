5e45706baeb8ca6886d3c8b599321203b61cfcba
Steve Occhipinti <dev@stevenocchipinti.com>
2018-06-27 00:04:03 +1000

    A demo of brute force with loops

--- toy_robot/app.rb
+++ toy_robot/app.rb
@@ -1,14 +1,25 @@
 require_relative './lib/game'
 
-game = Game.new(width: 5, height: 5)
-game.place_robot(x: 0, y: 0, direction: :east)
-game.place_target(x: 4, y: 2)
+WIDTH = 5
+HEIGHT = 5
 
-game.move_robot
-game.move_robot
-game.rotate_robot_left
-game.move_robot
-game.move_robot
-game.rotate_robot_right
-game.move_robot
-game.move_robot
+game = Game.new(width: WIDTH, height: HEIGHT, delay: 0.1)
+
+game.place_robot(x: 0, y: 0, direction: :east)
+game.place_target
+
+(HEIGHT).times do |row|
+  # Move along the row
+  (WIDTH - 1).times { game.move_robot }
+
+  # Move up to the next row
+  if row.even?
+    game.rotate_robot_left
+    game.move_robot
+    game.rotate_robot_left
+  else
+    game.rotate_robot_right
+    game.move_robot
+    game.rotate_robot_right
+  end
+end
